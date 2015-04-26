0046663f699de769bb849f8c8172898e350399e9
Jack Michaels <jack@bdbfx.com>
2015-04-26 20:54:56 +0300

    Fix pre-modal loading content display issue
    Close #20
    Fix issue where content is displayed before on the desktop before the
    dialog-window is able to display the content correctly.

--- parts/desktop.php
+++ parts/desktop.php
@@ -3,7 +3,7 @@
 	if ( have_posts() ) :
 		while ( have_posts() ) : the_post();
 	?>
-	<div id="post_<?php print intval( $post->ID ); ?>_content">
+	<div id="post_<?php print intval( $post->ID ); ?>_content" class="initial_content">
 		<article style="padding: 10px;">
 			<?php the_content(); ?>
 			<?php
